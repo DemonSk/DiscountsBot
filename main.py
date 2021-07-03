5bcd1ea882baa0466fba52c3df8d218957182344
Serhio <venna2013@gmail.com>
2021-07-03 15:36:31 +0300

    better code with sourcery

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ def func():
     soup = BeautifulSoup(response.text, 'lxml')
     items_name = soup.find_all('span', class_='jsx-4147154605 ProductTile__title')
     item_discount_price = soup.find_all('span', class_='jsx-3642073353 Price__value_caption Price__value_discount')
-    items = []
-    item_str = ''
-    for item in items_name:
-        items.append(item.text)
-    for i in range(len(items)):
-        item_str += f'{items[i]} {item_discount_price[i].text} грн.\n'
-    return item_str
+    items = [item.text for item in items_name]
+    return ''.join(
+        f'{items[i]} {item_discount_price[i].text} грн.\n'
+        for i in range(len(items))
+    )
