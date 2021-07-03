7225729156175a5cdfeb80262237e50a8dcf9532
Serhio <venna2013@gmail.com>
2021-07-03 18:25:59 +0300

    Change functions, added metro and urls

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+from config import auchanUrl, metroUrl
 
-def func():
-    url = 'https://auchan.zakaz.ua/ru/promotions/?category_id=low-price-auchan'
-    response = requests.get(url)
+language = 'ru'
+
+def getResult(shopUrl):
+    response = requests.get(f'{shopUrl}/{language}/promotions')
+    soup = BeautifulSoup(response.text, 'lxml')
+    categories = soup.find_all('a', class_='jsx-631741978 NestedList__itemTitle NestedList__itemTitle_bold')
+    global category_urls
+    category_urls = {category.text:category.get('href') for category in categories}
+    return [category.text for category in categories]
+
+
+def getShopCategories(shopName):
+    if shopName == 'Ашан':
+        return getResult(auchanUrl)
+    elif shopName == 'Метро':
+        return getResult(metroUrl)
+
+           
+def categoryPrice(category):
+    categoryUrl = category_urls.get(category)
+    response = requests.get(f'{auchanUrl}{categoryUrl}')
     soup = BeautifulSoup(response.text, 'lxml')
     items_name = soup.find_all('span', class_='jsx-4147154605 ProductTile__title')
     item_discount_price = soup.find_all('span', class_='jsx-3642073353 Price__value_caption Price__value_discount')
     items = [item.text for item in items_name]
     return ''.join(
-        f'{items[i]} {item_discount_price[i].text} грн.\n'
-        for i in range(len(items))
-    )
+            f'{items[i]} {item_discount_price[i].text} грн.\n'
+            for i in range(len(items))
+        )
